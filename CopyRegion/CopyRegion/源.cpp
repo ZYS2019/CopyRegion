f3eddac56334d10d7d3ef7d458a3fd55edc20731
Zhou Yisha <770967573@qq.com>
2019-03-14 10:55:14 +0800

    无修改

--- CopyRegion/CopyRegion/源.cpp
+++ CopyRegion/CopyRegion/源.cpp
@@ -3,7 +3,7 @@
 using namespace cv;
 int main()
 {
-	Mat src = imread("C:/Users/SZJ/Desktop/1.jpg");
+	Mat src = imread("C:\\Users\\SZJ\\Desktop\\1.jpg");
 	Mat dst = Mat::zeros(src.size(), src.type());
 	Mat mask = Mat::zeros(src.size(), CV_8U);
 	Mat dst2;
@@ -13,12 +13,12 @@ int main()
 	//»­Ô²
 	circle(mask, circleCenter, 200, Scalar(255), -1);
 	Rect rect(0, 0, 100, 200);
-	dst2 = src(rect);
+	//dst2 = src(rect);
 	src.copyTo(dst, mask);
 	imshow("mask", mask);
 	imshow("src", src);
-	imshow("dst", dst);
-	imshow("dst2", dst2);
+	
+	//imshow("dst2", dst2);
 	waitKey(0);
 	return 0;
 
